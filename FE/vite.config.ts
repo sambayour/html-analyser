cf6fe7080107e0934170c556410a09130ab510df
Samuel Olubayo <samuelolubayo@gmail.com>
2024-12-08 19:35:38 +0100

    add unit test using vitest

--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -1,7 +1,20 @@
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { configDefaults } from "vitest/config";
+import vuePlugin from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vuePlugin()],
+  test: {
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "e2e/*"],
+    root: fileURLToPath(new URL("./", import.meta.url)),
+    transformMode: {
+      web: [/\.[jt]sx$/],
+    },
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+    },
+  },
 });
